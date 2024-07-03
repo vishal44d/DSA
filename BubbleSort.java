cde694b8566a27c36a1d3eaa2ba63b76943d6775
Vishal Diwakar <98634369+vishal44d@users.noreply.github.com>
2024-07-03 21:21:52 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class BubbleSort {
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args){
+        int[] arr= {1, 5, 2, 3, 4, 7,12,8,9,4};
+        BubbleSortDemo(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void BubbleSortDemo(int[] arr){
+
+        boolean swap;
+        for(int i=0;i<arr.length-1;i++){
+            swap=false;
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    swap=true;
+                }
+            }
+            if(!swap) return;
+        }
+
+    }
 }
